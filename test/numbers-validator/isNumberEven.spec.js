f145a7b49044d1916d1e2f95e5f7d4b8223b7312
hroyero <hroyero22@gmail.com>
2024-05-14 19:24:19 -0500

    100% test coverage

--- test/numbers-validator/isNumberEven.spec.js
+++ test/numbers-validator/isNumberEven.spec.js
@@ -1,49 +1,55 @@
 // Importing the NumbersValidator class from the relative path '../app/numbers-validator'
 // so we can test its methods.
-import {NumbersValidator} from '../../app/numbers_validator.js';
-import { expect } from 'chai';
+import NumbersValidator from '../../app/numbers_validator.js'
+import { expect }  from 'chai';
 
-// 'describe' is used to group related tests together into a test suite.
-// Here, it describes a suite of tests for the 'isNumberEven' method of NumbersValidator class.
+
 describe('isNumberEven positive test', () => {
-  // Declaring a variable 'validator' outside of the 'beforeEach' and 'it' blocks
-  // to make it accessible throughout this describe block.
+
   let validator;
 
-  // 'beforeEach' is a hook that runs before each test ('it' block) within this 'describe' block.
-  // It's usually used for setting up the test environment.
   beforeEach(()=>{
-    // Instantiates a new NumbersValidator object before each test and assigns it to 'validator'
     validator = new NumbersValidator();
-
   });
 
-  // 'afterEach' is a hook that runs after each test. It is often used for cleanup activities.
   afterEach(()=>{
-    // Sets the validator variable to null to clean up memory after each test
     validator = null;
   });
 
-  // 'it' is used for individual test cases - it includes the actual test.
-  // The string argument describes what the test should do.
-  it('should return true when provided with an even number', ()=>{
-    // Using 'expect' to assert that the 'isNumberEven' method returns true when
-    // passed the even number 4. The '.to.be.equal(true)' is the actual assertion check.
-    const validationResult = validator.isNumberEven(4);
-    expect(validationResult).to.be.equal(true);
+
+  it('should check if a number is even', ()=>{
+
+    expect(validator.isNumberEven(4)).to.be.equal(true);
+    expect(validator.isNumberEven(3)).to.be.equal(false);
+    expect(() => validator.isNumberEven('4')).to.throw();
+
   });
 
-  it('should return false when provided with an even number', ()=>{
-    // Using 'expect' to assert that the 'isNumberEven' method returns true when
-    // passed the even number 4. The '.to.be.equal(true)' is the actual assertion check.
-    const validationResult = validator.isNumberEven(5);
-    expect(validationResult).to.be.equal(false);
+  it('should return an array of even numbers', () => {
+    const arrayOfNumbers = [1, 2, 3, 4, 5];
+    const arrayOfString = ["1", "2", "3", "4", "5"];
+
+    expect(validator.getEvenNumbersFromArray(arrayOfNumbers)).to.include.members([2, 4]);
+    expect(() => validator.getEvenNumbersFromArray(arrayOfString)).to.throw();
+
   });
-/*
-  it('shloud throw an erro when provided a string', () => {
-    expect(()=>{
-      validator.isNumberEven('4')
-    });
+
+  it('should check if all elements in an array are numbers', () => {
+    const arrayOfNumbers = [1, 2, 3, 4, 5];
+    const notArray = "I am not an array";
+
+    expect(validator.isAllNumbers(arrayOfNumbers)).to.be.equal(true);
+    expect(() => validator.isAllNumbers(notArray)).to.throw();
+
   });
-  */
+
+
+
+  it('should check if a value is an integer', () => {
+    expect(validator.isInteger(5)).to.be.equal(true);
+    expect(validator.isInteger(5.5)).to.be.equal(false);
+    expect(() => validator.isInteger(true)).to.throw();
+  });
+
 });
+
